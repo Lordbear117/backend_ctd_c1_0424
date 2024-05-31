a018effc955c014f83e99f9549d3d0f899b94d1a
lumurga <mlucianamurga@gmail.com>
2024-05-30 21:01:28 -0300

    Implementacion de model mapper en servicio de Paciente

--- clinica-odontologica/clinica-odontologica/src/main/java/com/backend/clinica_odontologica/ClinicaOdontologicaApplication.java
+++ clinica-odontologica/clinica-odontologica/src/main/java/com/backend/clinica_odontologica/ClinicaOdontologicaApplication.java
@@ -1,8 +1,10 @@
 package com.backend.clinica_odontologica;
 
 import com.backend.clinica_odontologica.dbconnection.H2Connection;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClinicaOdontologicaApplication {
@@ -13,5 +15,12 @@ public class ClinicaOdontologicaApplication {
 
 		SpringApplication.run(ClinicaOdontologicaApplication.class, args);
 	}
+
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
 
 }
--- clinica-odontologica/clinica-odontologica/src/main/java/com/backend/clinica_odontologica/service/impl/PacienteService.java
+++ clinica-odontologica/clinica-odontologica/src/main/java/com/backend/clinica_odontologica/service/impl/PacienteService.java
@@ -1,38 +1,65 @@
 package com.backend.clinica_odontologica.service.impl;
 
+import com.backend.clinica_odontologica.dto.entrada.DomicilioEntradaDto;
 import com.backend.clinica_odontologica.dto.entrada.PacienteEntradaDto;
 import com.backend.clinica_odontologica.dto.salida.PacienteSalidaDto;
+import com.backend.clinica_odontologica.entity.Domicilio;
 import com.backend.clinica_odontologica.entity.Paciente;
 import com.backend.clinica_odontologica.repository.IDao;
 import com.backend.clinica_odontologica.service.IPacienteService;
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 
 import java.util.List;
 
+
 @Service
 public class PacienteService implements IPacienteService {
     //se mapea de DTO a entidad y viceversa
+    private final Logger LOGGER = LoggerFactory.getLogger(PacienteService.class);
     private IDao<Paciente> pacienteIDao;
+    private final ModelMapper modelMapper;
 
-    public PacienteService(IDao<Paciente> pacienteIDao) {
+    public PacienteService(IDao<Paciente> pacienteIDao, ModelMapper modelMapper) {
         this.pacienteIDao = pacienteIDao;
+        this.modelMapper = modelMapper;
+        configureMapping();
     }
 
     @Override
     public PacienteSalidaDto registrarPaciente(PacienteEntradaDto pacienteEntradaDto) {
         //logica de negocio
         //mapeo de dto a entidad
-        //pacienteIDao.registrar(paciente);
+        LOGGER.info("PacienteEntradaDto: " + pacienteEntradaDto);
+        Paciente paciente = modelMapper.map(pacienteEntradaDto, Paciente.class);
+        LOGGER.info("PacienteEntidad: " + paciente);
+        //Paciente pacienteRegistrado = pacienteIDao.registrar(paciente);
         //mapeo de entidad a dto
-        return null;
+        PacienteSalidaDto pacienteSalidaDto = modelMapper.map(pacienteIDao.registrar(paciente), PacienteSalidaDto.class);
+        LOGGER.info("PacienteSalidaDto: " + pacienteSalidaDto);
+        return pacienteSalidaDto;
     }
 
     @Override
     public List<PacienteSalidaDto> listarPacientes() {
-        //pacienteIDao.listarTodos()
-
         //mapeo de lista de entidades a lista de dtos
-        return null;
+        List<PacienteSalidaDto> pacientes = pacienteIDao.listarTodos()
+                .stream()
+                .map(paciente -> modelMapper.map(paciente, PacienteSalidaDto.class))
+                .toList();
+        LOGGER.info("Listado de todos los pacientes: {}", pacientes);
+
+        return pacientes;
+    }
+
+
+    private void configureMapping(){
+        modelMapper.typeMap(PacienteEntradaDto.class, Paciente.class)
+                .addMappings(mapper -> mapper.map(PacienteEntradaDto::getDomicilioEntradaDto, Paciente::setDomicilio));
+        modelMapper.typeMap(Paciente.class, PacienteSalidaDto.class)
+                .addMappings(mapper -> mapper.map(Paciente::getDomicilio, PacienteSalidaDto::setDomicilioSalidaDto));
     }
 }
